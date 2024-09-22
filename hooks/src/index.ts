bcb8a01547c356174365d0a712ee1a60194a3f50
Somu878 <myemail>
2024-09-23 00:44:32 +0530

    add data schema

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,13 +1,31 @@
 import express from "express";
-
+import { PrismaClient } from "@prisma/client";
 const app = express();
-
-app.get("/hooks/catch/:userid/:zapid", (req, res) => {
+const client = new PrismaClient()
+app.get("/hooks/catch/:userid/:zapid",async (req, res) => {
     const { userid, zapid } = req.params;
     // const secret = req.headers["x-secret"];
     const { body } = req;
-    console.log(userid, zapid, body);
-    res.send("ok");
+    await client.$transaction(async tx=>{
+        const run=await client.zapRun.create({
+            data: {
+                zapId: zapid,
+                
+                metadata:{
+                    userID:userid,
+                    testUser:"Somu Kandula"
+                }
+            }
+        });
+        await client.zapRunOutbox.create({
+            data:{
+                zapRunId:run.id
+            }
+        })
+
+
+    })
+    return res.send("ok");
 });
 
 app.listen(3000, () => {
